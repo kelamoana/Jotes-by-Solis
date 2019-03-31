0965134501f5e931bd21d7873e9b19e233635316
Jennifer Chau <jmchau@uci.edu>
2019-03-31 10:26:52 -0700

    Updated

--- app/natural_lang_int.py
+++ app/natural_lang_int.py
@@ -7,6 +7,7 @@
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+import requests
 
 def natural_language_api(text):
     client = language.LanguageServiceClient()
@@ -20,7 +21,29 @@ def natural_language_api(text):
         lst.append(entity.name)
     return lst
 
+def wikipedia(lst):
+    S = requests.Session()
+    resources = []
+    URL = "https://en.wikipedia.org/w/api.php"
+    for word in lst:
+        SEARCHTERM = word
+
+        PARAMS = {
+            'action': "opensearch",
+            'search': SEARCHTERM,
+            'limit': 1,
+
+            'format': "json"
+        }
+
+        R = S.get(url=URL, params=PARAMS)
+        DATA = R.json()
+        if DATA[3]!=[]:
+            resources.append(DATA[3][0])
+    return resources
+
+
 
 if __name__ == '__main__':
     file = 'gs://petersaudiofiles/bettersong.flac'
-    print(natural_language_api(long_speech_to_text.transcribe_gcs(file)))
+    #print(natural_language_api(long_speech_to_text.transcribe_gcs(file)))
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template, redirect, url_for
 from app.oxfordapi import create_def_dict
 from app.long_speech_to_text import transcribe_gcs
-from app.natural_lang_int import natural_language_api
+from app.natural_lang_int import natural_language_api, wikipedia
 
 
 @app.route('/')
@@ -13,7 +13,7 @@ def index():
     transcription = transcribe_gcs(file_name)
     keywords = []
     words = natural_language_api(transcription)
-    resources = []
+    resources = wikipedia(words)[:5]
 
     for word in words:
         if len(keywords) == 12:
