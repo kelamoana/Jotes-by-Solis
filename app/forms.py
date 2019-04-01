f982c8f2e8fd72ea222259545ead83d2c8d7890f
Jennifer Chau <36621259+jennichaau@users.noreply.github.com>
2019-04-01 09:14:42 -0700

    Delete forms.py

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class SearchForm(FlaskForm):
-    ingredients = StringField('Ingredients', validators=[DataRequired()])
-    #sorting = RadioField('Sort by: ', choices=[('Highest Ratings','description'),('Trendiness','whatever')])
-    submit = SubmitField('Search Recipes')
